c467cfeffe232507ba10486522c1c79928c601f2
Aditya Gupta <cu240cs290187@coeruniversity.ac.in>
2026-08-04 20:28:23 +0530

    Update API endpoint for Gemini model

--- script.js
+++ script.js
@@ -390,7 +390,7 @@ childList:true
 async function askGemini(promptText){
 alert(API_KEY);
 const response = await fetch(
-"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" + API_KEY,
+"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=" + API_KEY,
 {
 method:"POST",
 
