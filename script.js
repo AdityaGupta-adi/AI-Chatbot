6ca5cf01f7850b1d6f8f94cdf922c33300ae898b
Aditya Gupta <cu240cs290187@coeruniversity.ac.in>
2026-08-04 20:23:26 +0530

    Alert API key before making fetch request
    Added alert to display API key before fetching data.

--- script.js
+++ script.js
@@ -388,7 +388,7 @@ childList:true
 });
 
 async function askGemini(promptText){
-
+alert(API_KEY);
 const response = await fetch(
 "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" + API_KEY,
 {
